7d1f4812efb5a53a108050114fd8d0831a3d131b
Hayden Terek <91907118+Haydenterek@users.noreply.github.com>
2021-10-19 23:00:22 -0400

    Add files via upload

--- dosomething.py
+++ dosomething.py
@@ -1,5 +1,14 @@
+#Asks the user for their name and stores it in name
 name = input("Name = ")
+
+#Asks the user for a number and stores it in num1
 num1 = int(input("Num1 = "))
+
+#Asks the user for a second number and stores it in num2
 num2 = int(input("Num2 = "))
+
+#Adds the two numbers and divides by 2 to get the average. Stored in avg
 avg = float(num1 + num2)/2
+
+#Prints the average back to the user
 print("Average = ", avg)
